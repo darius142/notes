66ba754de26f0ce2f3ac7686114587e5febf6468
Volodymyr Padovsky <vld06@ukr.net>
2017-12-22 23:38:24 +0200

    updated

--- react.js
+++ react.js
@@ -45,3 +45,27 @@ this.state = { mood: 'decent' };	// записати state(стан) власт�
 {this.state.mood} // прочитати властивість
 
 super(props) //
+
+// Events
+var Counter = React.createClass({
+  incrementCount: function(){
+    this.setState({
+      count: this.state.count + 1
+    });
+  },
+  getInitialState: function(){
+     return {
+       count: 0
+     }
+  },
+  render: function(){
+    return (
+      <div class="my-component">
+        <h1>Count: {this.state.count}</h1>
+        <button type="button" onClick={this.incrementCount}>Increment</button>
+      </div>
+    );
+  }
+});
+
+React.render(<Counter/>, document.getElementById('myDiv'));
